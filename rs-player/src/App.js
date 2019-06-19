8b6f6fbf7b39a0012a8761e34c826a10df8fe435
jerryCjr <chengjinrui@babyfs.cn>
2019-06-19 19:02:51 +0800

    [react] rs-player init

---
+++ rs-player/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Player from './Player.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Player></Player>
+    </div>
+  );
+}
+
+export default App;
---
+++ rs-player/src/Player.js
@@ -0,0 +1,92 @@
+import bindAll from 'lodash.bindall';
+import React from 'react';
+import VM from 'scratch-vm';
+import Renderer from 'scratch-render';
+import Storage from 'scratch-storage';
+import './player.css'
+
+class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    bindAll(this, [
+      'attachMouseEvents'
+    ])
+    this.state = {
+      oW: 667,
+      oH: 375,
+      scratchData: null,
+      mouseDownTimeoutId: null,
+      mouseDownPosition: null,
+      isDragging: false,
+      dragOffset: null,
+      dragId: null,
+      colorInfo: null,
+      question: null
+    }
+
+  }
+
+  componentDidMount() {
+    this.canvas = document.getElementById('ocanvas');
+    this.renderer = new Renderer(this.canvas);
+    this.storage = new Storage();
+    console.log(this.renderer);
+    console.log(this.storage);
+    // this.storage = new Storage();
+    // this.audio = new AudioEngine();
+    this.vm = new VM();
+    // this.vm.attachStorage(this.storage)
+    // this.vm.attachRenderer(this.renderer);
+    // this.vm.attachAudioEngine(this.audio);
+    // this.vm.renderer.draw();
+    // this.vm.attachV2SVGAdapter(new V2SVGAdapter());
+    // this.vm.attachV2BitmapAdapter(new V2BitmapAdapter());
+  }
+
+  updateRect() {
+    this.rect = this.canvas.getBoundingClientRect();
+  }
+
+  attachMouseEvents(canvas) {
+    document.addEventListener('mousemove', this.onMouseMove);
+    document.addEventListener('mouseup', this.onMouseUp);
+    document.addEventListener('touchmove', this.onMouseMove);
+    document.addEventListener('touchend', this.onMouseUp);
+    canvas.addEventListener('mousedown', this.onMouseDown);
+    canvas.addEventListener('touchstart', this.onMouseDown);
+    canvas.addEventListener('wheel', this.onWheel);
+  }
+
+  detachMouseEvents(canvas) {
+    document.removeEventListener('mousemove', this.onMouseMove);
+    document.removeEventListener('mouseup', this.onMouseUp);
+    document.removeEventListener('touchmove', this.onMouseMove);
+    document.removeEventListener('touchend', this.onMouseUp);
+    canvas.removeEventListener('mousedown', this.onMouseDown);
+    canvas.removeEventListener('touchstart', this.onMouseDown);
+    canvas.removeEventListener('wheel', this.onWheel);
+  }
+
+  attachRectEvents() {
+    window.addEventListener('resize', this.updateRect);
+    window.addEventListener('scroll', this.updateRect);
+  }
+
+  detachRectEvents() {
+    window.removeEventListener('resize', this.updateRect);
+    window.removeEventListener('scroll', this.updateRect);
+  }
+
+  render() {
+    return (
+      <canvas
+        id='ocanvas'
+        ref="canvas"
+        width={this.state.oW}
+        height={this.state.oH}
+      />
+    )
+  }
+}
+
+export default Player;
